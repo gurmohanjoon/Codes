0e28db8520b8b30ddd0c5760b2ec174987425fb8
gurmohanjoon <gurmohanjoon4@gmail.com>
2021-09-23 19:02:15 +0530

    initial commit

--- linkedlist/ll1.java
+++ linkedlist/ll1.java
@@ -11,7 +11,6 @@ class ll1
         }
     }
     public static linkedlist midNode(linkedlist head) {
-    
         if(head==null || head.next==null)
         return head;
         linkedlist fast=head,slow=head;
@@ -22,6 +21,78 @@ class ll1
          }
          return slow;
      }
+     public static linkedlist reverseList(linkedlist head) {
+        if (head == null || head.next == null)
+            return head;
+            linkedlist prev = null, curr = head;
+        while (curr != null) {
+            linkedlist forw = curr.next; // backup
+            curr.next = prev; // link
+            prev = curr; // move
+            curr = forw;
+        }
+        return prev;
+    }
+    public boolean isPalindrome(linkedlist head) {
+        if(head==null && head.next == null)
+            return true;
+        boolean flag = true;
+        linkedlist mid =midNode(head);
+        linkedlist nhead=mid.next;
+        mid.next=null;
+        nhead = reverseList(nhead);
+        linkedlist c1=head,c2=nhead;
+        while(c1 != null && c2 != null)
+        {
+            if(c1.val != c2.val)
+            { 
+                flag=false;
+                break;
+            }
+            c1=c1.next;
+            c2=c2.next;
+        }
+        nhead = reverseList(nhead);
+        mid.next=nhead;
+        return flag;
+    }
+    public void fold(linkedlist head) {
+        if(head==null && head.next == null)
+            return;
+        boolean flag = true;
+        linkedlist mid =midNode(head);
+        linkedlist nhead=mid.next;
+        mid.next=null;
+        nhead = reverseList(nhead);
+        linkedlist c1=head,c2=nhead;
+        while(c1 != null && c2!= null)
+        {
+            linkedlist f1=c1.next,f2=c2.next;
+            c1.next=c2;
+            c2.next=f1;
+            c1=f1;
+            c2=f2;
+        }
+    }
+    public static void unfold(linkedlist head)
+    {
+         if(head == null && head.next ==null)
+         return ;
+         linkedlist c1=head,c2=head.next,nhead=c2;
+         while(c1!=null && c2!= null)
+         {
+             linkedlist f1=c2.next;
+             c1.next=f1;
+             c2.next=f1.next;
+
+             c1=f1;
+             c2=c1.next;
+         }
+         c1.next=null;
+         nhead=reverseList(nhead);
+         c1.next=nhead;
+    }
+    public static 
     public static void main(String[] args)
     {
           
